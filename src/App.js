86cc42056876cddc463dc20eed082b8d62901b7e
chandula <e16061@eng.pdn.ac.lk>
2022-06-27 10:06:19 +0530

    set the layout🎉

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Layout from './components/Layout'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Layout />} />
+      </Routes>     
     </div>
   );
 }
