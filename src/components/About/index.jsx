6388ef9a23828008dc11c856571b142c7a1247fe
chandula <e16061@eng.pdn.ac.lk>
2022-11-19 18:58:08 +0530

    update the content of the page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './index.scss'
 import { Link } from 'react-router-dom'
 import Profile from '../../assets/images/chandulaJPDM.jpg'
+import Resume from '../../assets/chandula_cv.pdf'
 
 const About = () => {
   return (
@@ -9,20 +10,24 @@ const About = () => {
         <div className="text-area">
             <h2>About me</h2>
             
-            <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML &amp; CSS!</p>
-            <p>Fast-forward to today, and I’ve had the privilege of working at Upstatement for a variety of clients.</p>
-            <p>I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node &amp; React.</p>
+            <p>Hello! My name is Madushan Chandula and I am a final year undergraduate at Department of Computer Engineering, University of Peradeniya. 
+              I am interested in Web development, Cloud computing, Cyber security and Network designing. I am very good at self learning and also I love to work in team enviroments.</p>
+            <p>I had a privillage of doing my intership at NCSU Univeristy of Peradeniya where I learn a lot about Web development, Network managment and many other things.</p>
+            <p>Fast forward to today I am currently working on a project with FORD motor company to build an ECU emulator for the Digital Twins in the car manufacturing industry.</p>
             <p>Here are a few technologies I’ve been working with recently:</p>
             
             <ul>
                 <li>JavaScript (ES6+)</li>
                 <li>TypeScript</li>
                 <li>React</li>
-                <li>Eleventy</li>
+                <li>Laravel</li>
                 <li>Node.js</li>
-                <li>WordPress</li>
+                <li>Azure</li>
+                <li>CSS</li>
+                <li>C++</li>
+                <li>Computer Architecture</li>
             </ul>
-            <Link to='/resume' className='flat-button'>Download Resume</Link>
+            <Link to={Resume} className='flat-button' rel="noreferrer" target="_blank" download>Download Resume</Link>
         </div>
         <div className="image-area">
             <img src={Profile} alt="" />
