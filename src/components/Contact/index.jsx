01acc46a04c8571b1b146e3a462f6f5cb7385658
chandula <e16061@eng.pdn.ac.lk>
2022-11-19 18:59:15 +0530

    update the content of the page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
     <div className='container contact-page'>
       <h1>What's Next?</h1>
       <h2>Get In Touch</h2>
-      <p>Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+      <p>I’m currently looking for a new opportunity in software industry, If you like my resume feel free to drop a message. My inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
 
       <a href='mailto:chandula12d@gmail.com' target='_blank'>Say Hello</a>
     </div>
