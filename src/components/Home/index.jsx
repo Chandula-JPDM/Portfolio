2a75cea9d05a748faa42092ebb43849ac92530df
chandula <e16061@eng.pdn.ac.lk>
2022-11-19 17:36:34 +0530

    modify content of the page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,8 +7,8 @@ const index = () => {
     <div className='container home-page'>
         <h1>Hi, my name is</h1>
         <h2>Chandula J.P.D.M.</h2>
-        <h3>I build things for the web.</h3>
-        <p>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatemnt.</p>
+        <h3>I am a Computer Engineer.</h3>
+        <p>I'm an Undergraduate at Department of Computer Engineering, University of Peradeniya. Currently, I’m working on a research about Configurable ECU Emulator for Creating Digital Twins in the Car Manufacturing Industry.</p>
         
         <Link to='contact' className='flat-button'>Contact me</Link>
     </div>
